09496ae1bd0ac2a6433577103ac7a673d7831c1a
Cristian Deleon <libaration@gmail.com>
2020-07-11 02:01:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[grace_hopper][known_for]
 
 end
 
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
         }
      }
 
-		 programmer_hash[grace_hopper][known_for]
+
 end
 
 def dennis_ritchies_language
